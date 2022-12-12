8698b5bb74cdba3d7f64c2a620027d1e84b5cff9
michalswig <mike@data.pl>
2022-12-12 17:29:01 +0100

    add exception handling

--- src/com/mike/evaluator/AdviseSimpleEvaluator.java
+++ src/com/mike/evaluator/AdviseSimpleEvaluator.java
@@ -7,111 +7,114 @@ import java.time.format.DateTimeParseException;
 import java.util.Map;
 import java.util.Set;
 
-import static com.mike.evaluator.DataType.INTEGER;
-
 public class AdviseSimpleEvaluator implements Evaluator {
 
     @Override
     public boolean evaluate(Context context, Expression expression) {
-
-        DateTimeFormatter localDateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        DateTimeFormatter localDateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-
-        Map<String, VariableValue> variables = context.getVariables();
-
-        validate(variables, expression);
-
-        String value = expression.getValue();
-
-        String[] expressions = value.trim().split(" ");
-        String keyExpression;
-        String operator;
-        String valueExpression;
-
-        if (expressions.length == 3) {
-            keyExpression = expressions[0].trim();
-            operator = expressions[1].trim();
-            valueExpression = expressions[2].trim();
-        } else {
-            keyExpression = expressions[0].trim();
-            operator = expressions[1].trim();
-            valueExpression = expressions[2].trim() + " " + expressions[3].trim();
-        }
-
-        Set<String> keys = variables.keySet();
-        for (String key : keys) {
-            if (key.equals(keyExpression)) {
-                int keyVariable = 0;
-                int valueExpressionData = 0;
-                LocalDate keyLocalDate;
-                LocalDate expressionDate;
-                LocalDateTime keyLocalDateTime;
-                LocalDateTime expressionDateTime;
-                DataType[] values = DataType.values();
-                DataType dataTypeByKey = variables.get(key).getDataType();
-                for (DataType dataType : values) {
-                    switch (dataTypeByKey) {
+        try {
+            DateTimeFormatter localDateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            DateTimeFormatter localDateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+            Map<String, VariableValue> variables = context.getVariables();
+            validate(variables, expression);
+            String[] expressions = getStrings(expression);
+            String keyExpression = expressions[0].trim();
+            String operator = expressions[1].trim();
+            String valueExpression = expressions.length == 3 ? expressions[2].trim() : expressions[2].trim() + " " + expressions[3].trim();
+            Set<String> keys = variables.keySet();
+            for (String key : keys) {
+                if (key.equals(keyExpression)) {
+                    switch (variables.get(key).getDataType()) {
                         case INTEGER -> {
-                            keyVariable = Integer.parseInt(variables.get(key).getValue());
-                            valueExpressionData = Integer.parseInt(valueExpression);
-                            if (operator.equals(Operator.EQUAL.getOperator()) && keyVariable == valueExpressionData)
-                                return true;
-                            else if (operator.equals(Operator.MORE.getOperator()) && keyVariable > valueExpressionData)
-                                return true;
-                            else if (operator.equals(Operator.LESS.getOperator()) && keyVariable < valueExpressionData)
-                                return true;
-                            else if (operator.equals(Operator.MORE_EQUAL.getOperator()) && keyVariable >= valueExpressionData)
-                                return true;
-                            else if (operator.equals(Operator.LESS_EQUAL.getOperator()) && keyVariable <= valueExpressionData)
-                                return true;
-                            else if (operator.equals(Operator.NOT_EQUAL.getOperator()) && keyVariable != valueExpressionData)
-                                return true;
-                            else {
-                                return false;
-                            }
+                            return matchContextInteger(variables, operator, valueExpression, key);
                         }
                         case DATE -> {
-                            keyLocalDate = LocalDate.parse(variables.get(key).getValue());
-                            expressionDate = LocalDate.parse(valueExpression, localDateFormatter);
-                            if (operator.equals(Operator.EQUAL.getOperator()) && keyLocalDate.isEqual(expressionDate))
-                                return true;
-                            else if (operator.equals(Operator.NOT_EQUAL.getOperator()) && !keyLocalDate.isEqual(expressionDate))
-                                return true;
-                            else if (operator.equals(Operator.MORE.getOperator()) && !keyLocalDate.isAfter(expressionDate))
-                                return true;
-                            else if (operator.equals(Operator.LESS.getOperator()) && !keyLocalDate.isBefore(expressionDate))
-                                return true;
-                            else {
-                                return false;
-                            }
+                            return matchContextDate(localDateFormatter, variables, operator, valueExpression, key);
                         }
                         case DATE_TIME -> {
-                            keyLocalDateTime = LocalDateTime.parse(variables.get(key).getValue(), localDateTimeFormatter);
-                            expressionDateTime = LocalDateTime.parse(valueExpression, localDateTimeFormatter);
-                            if (operator.equals(Operator.EQUAL.getOperator()) && keyLocalDateTime.isEqual(expressionDateTime))
-                                return true;
-                            else if (operator.equals(Operator.NOT_EQUAL.getOperator()) && !keyLocalDateTime.isEqual(expressionDateTime))
-                                return true;
-                            else if (operator.equals(Operator.MORE.getOperator()) && keyLocalDateTime.isAfter(expressionDateTime))
-                                return true;
-                            else if (operator.equals(Operator.LESS.getOperator()) && keyLocalDateTime.isBefore(expressionDateTime))
-                                return true;
-                            else {
-                                return false;
-                            }
+                            return matchContextDateTime(localDateTimeFormatter, variables, operator, valueExpression, key);
                         }
                         case STRING -> {
-                            if (operator.equals(Operator.EQUAL.getOperator()) && valueExpression.equals(variables.get(key).getValue()))
-                                return true;
-                            else {
-                                return false;
-                            }
+                            return matchContextString(variables, operator, valueExpression, key);
                         }
                     }
                 }
             }
+            return false;
+        } catch (DateTimeParseException | NumberFormatException e) {
+            throw new EvaluationException(e.getMessage());
         }
-        return false;
+    }
+
+    private static String[] getStrings(Expression expression) {
+        String value = expression.getValue();
+        String[] expressions = value.trim().split(" ");
+        return expressions;
+    }
+
+    private static boolean matchContextString(Map<String, VariableValue> variables, String operator, String valueExpression, String key) {
+        if (operator.equals(Operator.EQUAL.getOperator()) && valueExpression.equals(variables.get(key).getValue()))
+            return true;
+        else {
+            return false;
+        }
+    }
+
+    private static boolean matchContextDateTime(DateTimeFormatter localDateTimeFormatter, Map<String, VariableValue> variables, String operator, String valueExpression, String key) {
+        LocalDateTime expressionDateTime;
+        LocalDateTime keyLocalDateTime;
+        keyLocalDateTime = LocalDateTime.parse(variables.get(key).getValue(), localDateTimeFormatter);
+        expressionDateTime = LocalDateTime.parse(valueExpression, localDateTimeFormatter);
+        if (operator.equals(Operator.EQUAL.getOperator()) && keyLocalDateTime.isEqual(expressionDateTime))
+            return true;
+        else if (operator.equals(Operator.NOT_EQUAL.getOperator()) && !keyLocalDateTime.isEqual(expressionDateTime))
+            return true;
+        else if (operator.equals(Operator.MORE.getOperator()) && keyLocalDateTime.isAfter(expressionDateTime))
+            return true;
+        else if (operator.equals(Operator.LESS.getOperator()) && keyLocalDateTime.isBefore(expressionDateTime))
+            return true;
+        else {
+            return false;
+        }
+    }
+
+    private static boolean matchContextDate(DateTimeFormatter localDateFormatter, Map<String, VariableValue> variables, String operator, String valueExpression, String key) {
+        LocalDate keyLocalDate;
+        LocalDate expressionDate;
+        keyLocalDate = LocalDate.parse(variables.get(key).getValue());
+        expressionDate = LocalDate.parse(valueExpression, localDateFormatter);
+        if (operator.equals(Operator.EQUAL.getOperator()) && keyLocalDate.isEqual(expressionDate))
+            return true;
+        else if (operator.equals(Operator.NOT_EQUAL.getOperator()) && !keyLocalDate.isEqual(expressionDate))
+            return true;
+        else if (operator.equals(Operator.MORE.getOperator()) && keyLocalDate.isAfter(expressionDate))
+            return true;
+        else if (operator.equals(Operator.LESS.getOperator()) && keyLocalDate.isBefore(expressionDate))
+            return true;
+        else {
+            return false;
+        }
+    }
+
+    private static boolean matchContextInteger(Map<String, VariableValue> variables, String operator, String valueExpression, String key) {
+        int keyVariable;
+        int valueExpressionData;
+        keyVariable = Integer.parseInt(variables.get(key).getValue());
+        valueExpressionData = Integer.parseInt(valueExpression);
+        if (operator.equals(Operator.EQUAL.getOperator()) && keyVariable == valueExpressionData)
+            return true;
+        else if (operator.equals(Operator.MORE.getOperator()) && keyVariable > valueExpressionData)
+            return true;
+        else if (operator.equals(Operator.LESS.getOperator()) && keyVariable < valueExpressionData)
+            return true;
+        else if (operator.equals(Operator.MORE_EQUAL.getOperator()) && keyVariable >= valueExpressionData)
+            return true;
+        else if (operator.equals(Operator.LESS_EQUAL.getOperator()) && keyVariable <= valueExpressionData)
+            return true;
+        else if (operator.equals(Operator.NOT_EQUAL.getOperator()) && keyVariable != valueExpressionData)
+            return true;
+        else {
+            return false;
+        }
     }
 
     private static void validate(Map<String, VariableValue> variables, Expression expression) {
--- src/com/mike/evaluator/Main.java
+++ src/com/mike/evaluator/Main.java
@@ -6,26 +6,26 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
-        Expression expressionText = new Expression("customer.name == Jolka");
-        Expression expressionInteger = new Expression("customer.height >= 190");
+        Expression expressionText = new Expression("customer.name == Adam");
+        Expression expressionInteger = new Expression("customer.height <= 178");
         Expression expressionDateBirth = new Expression("customer.dateOfBirth > 1960-01-15");
-        Expression expressionDateBirthTime = new Expression("customer.dateOfBirthTime > 1982-01-15 01:40");
+        Expression expressionDateBirthTime = new Expression("customer.dateOfBirthTime < 1982-01-15 01:40");
         Evaluator evaluator = new AdviseSimpleEvaluator();
         Map<String, VariableValue> variables = new HashMap<>();
-        variables.put("customer.name", new VariableValue("Adam", DataType.STRING));
-        variables.put("customer.height", new VariableValue("205", DataType.INTEGER));
-        variables.put("customer.dateOfBirth", new VariableValue("2021-01-01", DataType.DATE));
-        variables.put("customer.dateOfBirthTime", new VariableValue("2023-01-01 00:01", DataType.DATE_TIME));
+        variables.put("customer.name", new VariableValue("Wojtek", DataType.STRING));
+        variables.put("customer.height", new VariableValue("170", DataType.INTEGER));
+        variables.put("customer.dateOfBirth", new VariableValue("1980-01-01", DataType.DATE));
+        variables.put("customer.dateOfBirthTime", new VariableValue("1982-01-15 01:40", DataType.DATE_TIME));
 
         Context context = new Context(variables);
         boolean evaluateText = evaluator.evaluate(context, expressionText);
         System.out.println(expressionText  + " and value: " + "'" + variables.get("customer.name").getValue() + "' is " + evaluateText);
         boolean evaluateInteger = evaluator.evaluate(context, expressionInteger);
-        System.out.println("customer.height evaluate: " + evaluateInteger);
+        System.out.println(expressionInteger  + " and value: " + "'" + variables.get("customer.height").getValue() + "' is " + evaluateInteger);
         boolean evaluateDateBirth = evaluator.evaluate(context, expressionDateBirth);
-        System.out.println("customer.dateOfBirth evaluate: " + evaluateDateBirth);
+        System.out.println(expressionDateBirth  + " and date: " + "'" + variables.get("customer.dateOfBirth").getValue() + "' is " + evaluateDateBirth);
         boolean evaluateDateBirthTime = evaluator.evaluate(context, expressionDateBirthTime);
-        System.out.println("customer.dateOfBirthTime evaluate: " + evaluateDateBirthTime);
+        System.out.println(expressionDateBirthTime  + " and date: " + "'" + variables.get("customer.dateOfBirthTime").getValue() + "' is " + evaluateDateBirthTime);
 
 
     }
