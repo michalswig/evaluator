18e1ed2f9bb9c74382a61f20649819cb6d4fd452
michalswig <mike@data.pl>
2023-02-06 10:25:50 +0100

    prepare Expression with values

--- src/main/java/pl/com/mike/evaluator/RPNEvaluator.java
+++ src/main/java/pl/com/mike/evaluator/RPNEvaluator.java
@@ -1,12 +1,11 @@
 package pl.com.mike.evaluator;
 
-import pl.com.mike.evaluator.convert.Operators;
 import pl.com.mike.evaluator.convert.RPNConvert;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeParseException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RPNEvaluator implements Evaluator {
 
@@ -17,8 +16,23 @@ public class RPNEvaluator implements Evaluator {
 
         String[] tokens = RPNConvert.convertInfixToRPN(getTokensFromExpression(expression));
 
+        Map<DataType, String> dataTypeWithValue = prepareExpression(tokens, context.getVariables());
+
         return false;
 
+    }
+
+    private static Map<DataType, String> prepareExpression(String[] tokens, Map<String, VariableValue> variables) {
+        Map<DataType, String> valueDataTypes = new HashMap<>();
+        for (String key : variables.keySet()) {
+            for (int i = 0; i < tokens.length; i++) {
+                if (tokens[i].equals(key)) {
+                    tokens[i] = variables.get(key).getValue();
+                    valueDataTypes.put(variables.get(key).getDataType(), tokens[i]);
+                }
+            }
+        }
+        return valueDataTypes;
     }
 
     private static List<String> getTokensFromExpression(Expression expression) {
